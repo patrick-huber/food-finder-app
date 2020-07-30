87a13827242c98f0e1a8a0bfe5b5e573a02c757f
Patrick Huber <patrick.huber1@gmail.com>
2020-07-30 10:42:37 -0500

    Copy updates for signup account exists error

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -65,11 +65,8 @@ const INITIAL_STATE = {
 const ERROR_CODE_ACCOUNT_EXISTS = 'auth/email-already-in-use';
 
 const ERROR_MSG_ACCOUNT_EXISTS = `
-  An account with this E-Mail address already exists.
-  Try to login with this account instead. If you think the
-  account is already used from one of the social logins, try
-  to sign in with one of them. Afterward, associate your accounts
-  on your personal account page.
+  An account with this email address already exists.
+  Try to login with this account instead.
 `;
 
 class SignUpFormBase extends Component {
